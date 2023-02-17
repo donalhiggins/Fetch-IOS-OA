61d83d36661e58900f848c9a7aa25eba434facef
Donal Higgins <dvh121103@gmail.com>
2023-02-17 14:42:12 -0600

    Initial Commit

---
+++ fetchOA/fetchOAApp.swift
@@ -0,0 +1,17 @@
+//
+//  fetchOAApp.swift
+//  fetchOA
+//
+//  Created by Donal Higgins on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct fetchOAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
